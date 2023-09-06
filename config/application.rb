6b01291f676c2c5e87d297cf51a68111e6f685d0
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-06 11:27:24 +0100

    Remove long lived creds

--- config/application.rb
+++ config/application.rb
@@ -34,14 +34,14 @@ module FbUserFilestore
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.x.s3_internal_bucket_config = {
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-    }
-    config.x.s3_external_bucket_config = {
-      access_key_id: ENV.fetch('AWS_S3_EXTERNAL_BUCKET_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_S3_EXTERNAL_BUCKET_SECRET_ACCESS_KEY')
-    }
+    # config.x.s3_internal_bucket_config = {
+    #   access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    #   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    # }
+    # config.x.s3_external_bucket_config = {
+    #   access_key_id: ENV.fetch('AWS_S3_EXTERNAL_BUCKET_ACCESS_KEY_ID'),
+    #   secret_access_key: ENV.fetch('AWS_S3_EXTERNAL_BUCKET_SECRET_ACCESS_KEY')
+    # }
     config.api_only = true
   end
 end
